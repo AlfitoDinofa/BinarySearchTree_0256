ecaea66d5f258fedf1286052679f7201debb9a8d
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-26 11:05:41 +0700

    menambahkan void search

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -45,5 +45,39 @@ public:
         node *parent = nullptr;
         node *currentnode = nullptr;
         
+        search(x, parent, currentnode);
+
+        if (parent == nullptr)
+        {
+            ROOT = newNode;
+            return;
+        }
+
+        if (x < parent->info)
+        {
+            parent->leftchild = newNode;
+            return;
+        }
+        else if (x > parent->info)
+        {
+            parent->rightchild = newNode;
+            return;
+        }
+    }
+
+    void search(int element, node *&parent, node *&currentnode)
+    {
+        currentnode = ROOT;
+        parent = nullptr;
+        while ((currentnode != nullptr) && (currentnode->info != element))
+        {
+            parent = currentnode;
+            if (element < currentnode->info)
+                currentnode = currentnode->leftchild;
+            else
+                currentnode = currentnode->rightchild;
+        }
+    }
+
 
 
