b7d2b2eb3ab7264cf9c737d862a30e2fde1e17cd
AlfitoDinofa <alfitodinofa2006@gmail.com>
2025-06-26 10:50:00 +0700

    assign value to the data

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class node{
+class node
+{
 public:
     int info;
     node *leftchild;
     node *rightchild;
-    
+
     node()
     {
-        leftchild = nullptr; //initialoze left child to the null
-        rightchild = nullptr; //initilozr right child to the null
+        leftchild = nullptr;  // initialoze left child to the null
+        rightchild = nullptr; // initilozr right child to the null
     }
-
-};  
+};
 
 class BinaryTree
 {
@@ -22,7 +22,7 @@ public:
 
     BinaryTree()
     {
-        ROOT = nullptr; //intializing root to null
+        ROOT = nullptr; // intializing root to null
     }
     void insert()
     {
@@ -30,7 +30,8 @@ public:
         cout << "masukkan nilai: ";
         cin >> x;
 
-        //step 1 : allocate memory for the new node
+        // step 1 : allocate memory for the new node
         node *newNode = new node();
 
-
+        // step 2 : assign value to the data field of new node
+        newNode->info = x;
